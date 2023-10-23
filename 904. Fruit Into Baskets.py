5d63bd6dad11e57a3d47bc159b7ffe01aede9b0a
Blacknahil <119704083+Blacknahil@users.noreply.github.com>
2023-10-23 14:40:06 +0300

    Create 904. Fruit Into Baskets.py

---
+++ 904. Fruit Into Baskets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        _max=start=0
+        count={}
+        for i, fruit in enumerate(fruits):
+            count[fruit]=count.get(fruit,0) + 1
+            while len(count) > 2:
+                count[fruits[start]]-=1
+                if count[fruits[start]]==0:
+                    del count[fruits[start]]
+                start+=1
+            _max=max(_max,i-start+1)
+        return _max
