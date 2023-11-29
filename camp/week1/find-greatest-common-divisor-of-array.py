1c34b26a1351018c400c5a7e4282d3dfaf42d50f
Nahom Garefo <119704083+Blacknahil@users.noreply.github.com>
2023-11-29 19:20:12 +0300

    Added solution for Find Greatest Common Divisor of Array

--- camp/week1/find-greatest-common-divisor-of-array.py
+++ camp/week1/find-greatest-common-divisor-of-array.py
@@ -1,14 +1,9 @@
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        min_num=float("inf")
-        max_num=0
-        for num in nums:
-            min_num=min(min_num,num)
-            max_num=max(max_num,num)
-        
+        min_num=min(nums)
+        max_num=max(nums)
         for div in range(min_num,0,-1):
-            if min_num % div==0 and max_num %div==0:
+            if max_num % div==0 and min_num %div==0:
                 return div
 
-        [2,5,6,9,10]
         
