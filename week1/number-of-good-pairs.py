00ff233a2c5633268124b60be32dedb6801521d3
Nahom Garefo <119704083+Blacknahil@users.noreply.github.com>
2023-12-04 09:32:46 +0300

    Added solution for Number of Good Pairs

---
+++ week1/number-of-good-pairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] != nums[j]:
+                    break
+                count+=1
+        return count
